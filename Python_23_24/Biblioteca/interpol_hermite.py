1a387fd01dcfc210b8c8b9fc546b80c5e34b404d
Jloked <jesusgzabril@gmail.com>
2023-12-04 14:49:04 +0100

    Interpol hermite

--- Python_23_24/Biblioteca/interpol_hermite.py
+++ Python_23_24/Biblioteca/interpol_hermite.py
@@ -23,7 +23,7 @@ def coef_hermite(x: np.ndarray, y: list):
         for j in range(aux[i], aux[i + 1]):
             nodos_rep[j - 1] = x[i]
 
-    for k in range(n):
+    for k in range(n):  # no va
         for i in range(m, -1, -1):
             for j in range(aux[i] - 2, max(aux[i - 1], k + 1) - 2, -1):
                 if nodos_rep[j - k] == nodos_rep[j]:
